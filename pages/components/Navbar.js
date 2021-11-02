27f2656873a9d1cf03f540b7cb464df4fb3455d2
Kenton Duprey <kentonduprey@icloud.com>
2021-11-02 18:13:56 -0400

    Adding navbar component

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,12 +1,82 @@
-import { Box, Heading } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { useColorModeValue } from "@chakra-ui/color-mode";
+import { Box, Heading, Flex, Text } from "@chakra-ui/layout";
 
 const Navbar = () => {
-  return (
-    <Box>
-      <Heading textAlign="center">Navbar</Heading>
-      <Box>Mobile</Box>
-    </Box>
-  );
+	return (
+		<Box display="flex" flexDir="column" alignItems="center">
+			<Heading textAlign="center">Navbar</Heading>
+
+			<Text textAlign="center" py={3}>
+				iPhone Portrait Navbar
+			</Text>
+			<Box
+				bg={useColorModeValue("brand.teal.50", "gray.900")}
+				h={"16"}
+				w="md"
+				display={"flex"}
+				alignItems="center"
+				justifyContent="space-evenly"
+			>
+				<Button bg="brand.teal.200">Item 1</Button>
+				<Button bg="brand.teal.200">Item 2</Button>
+				<Button bg="brand.teal.200">Item 3</Button>
+				<Button bg="brand.teal.200">Item 4</Button>
+				<Button bg="brand.teal.200">Item 5</Button>
+			</Box>
+			<Text textAlign="center" py={3}>
+				iPhone Landscape Navbar
+			</Text>
+			<Box
+				bg={useColorModeValue("brand.teal.50", "gray.900")}
+				h={"16"}
+				w="2xl"
+				display={"flex"}
+				alignItems="center"
+				justifyContent="space-evenly"
+			>
+				<Button bg="brand.teal.200">Item 1</Button>
+				<Button bg="brand.teal.200">Item 2</Button>
+				<Button bg="brand.teal.200">Item 3</Button>
+				<Button bg="brand.teal.200">Item 4</Button>
+				<Button bg="brand.teal.200">Item 5</Button>
+			</Box>
+			<Text textAlign="center" py={3}>
+				iPad Portrait Navbar
+			</Text>
+			<Box
+				bg={useColorModeValue("brand.teal.50", "gray.900")}
+				h={"16"}
+				w="3xl"
+				display={"flex"}
+				alignItems="center"
+				justifyContent="space-evenly"
+			>
+				<Button bg="brand.teal.200">Item 1</Button>
+				<Button bg="brand.teal.200">Item 2</Button>
+				<Button bg="brand.teal.200">Item 3</Button>
+				<Button bg="brand.teal.200">Item 4</Button>
+				<Button bg="brand.teal.200">Item 5</Button>
+			</Box>
+			<Text textAlign="center" py={3}>
+				iPad Landscape Navbar
+			</Text>
+			<Box
+				bg={useColorModeValue("brand.teal.50", "gray.900")}
+				h={"16"}
+				w="6xl"
+				display={"flex"}
+				alignItems="center"
+				justifyContent="space-evenly"
+			>
+				<Button bg="brand.teal.200">Item 1</Button>
+				<Button bg="brand.teal.200">Item 2</Button>
+				<Button bg="brand.teal.200">Item 3</Button>
+				<Button bg="brand.teal.200">Item 4</Button>
+				<Button bg="brand.teal.200">Item 5</Button>
+			</Box>
+		</Box>
+	);
 };
 
 export default Navbar;
